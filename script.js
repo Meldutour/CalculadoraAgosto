d6c9f817cdf44529e5fa275e129ba0b325ffd2f4
Melina Dutour <dutour_melina@hotmail.com>
2022-08-22 16:34:38 -0300

    agregamos funciones y quitamos el codigo js del html

--- script.js
+++ script.js
@@ -1,2 +1,78 @@
 document.querySelector('.resultado').innerHTML = ''
 document.querySelector('.sumatoria').innerHTML = ''
+
+function agregarNumeroCero() {
+    document.querySelector('.resultado').innerHTML += '0'
+}
+
+function agregarNumeroUno() {
+    document.querySelector('.resultado').innerHTML += '1'
+}
+
+function agregarNumeroDos() {
+    document.querySelector('.resultado').innerHTML += '2'
+}
+
+function agregarNumeroTres() {
+    document.querySelector('.resultado').innerHTML += '3'
+}
+
+function agregarNumeroCuatro() {
+    document.querySelector('.resultado').innerHTML += '4'
+}
+
+function agregarNumeroCinco() {
+    document.querySelector('.resultado').innerHTML += '5'
+}
+
+function agregarNumeroSeis() {
+    document.querySelector('.resultado').innerHTML += '6'
+}
+
+function agregarNumeroSiete() {
+    document.querySelector('.resultado').innerHTML += '7'
+}
+
+function agregarNumeroOcho() {
+    document.querySelector('.resultado').innerHTML += '8'
+}
+
+function agregarNumeroNueve() {
+    document.querySelector('.resultado').innerHTML += '9'
+}
+
+function limpiar() {
+    document.querySelector('.resultado').innerHTML = ''
+}
+
+function limpiarTodo() {
+    document.querySelector('.resultado').innerHTML = ''
+}
+
+function porcentaje() {
+    document.querySelector('.resultado').innerHTML += '%'
+}
+
+function dividir() {
+    document.querySelector('.resultado').innerHTML += '/'
+}
+
+function punto() {
+    document.querySelector('.resultado').innerHTML += '.'
+}
+
+function multiplicar() {
+    document.querySelector('.resultado').innerHTML += 'X'
+}
+
+function menos() {
+    document.querySelector('.resultado').innerHTML += '-'
+}
+
+function mas() {
+    document.querySelector('.resultado').innerHTML += '+'
+}
+
+function igual() {
+    document.querySelector('.resultado').innerHTML += '='
+}
